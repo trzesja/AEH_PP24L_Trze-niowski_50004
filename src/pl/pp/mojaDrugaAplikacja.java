5349122faa09284a023a8edcf629a002ae4a785c
Jakub <trzesja9342_aehit@students.vizja.pl>
2024-03-11 16:31:56 +0100

    Pierwszy zapis do repozytoriumLab2

---
+++ src/pl/pp/mojaDrugaAplikacja.java
@@ -0,0 +1,24 @@
+package pl.pp;
+
+import java.util.Scanner;
+
+public class mojaDrugaAplikacja {
+
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        int dwukrotnosc = 2 * x;
+
+        int kwadrat = x * x;
+
+        System.out.println("x = " + x);
+        System.out.println("Dwukrotność liczby x = " + dwukrotnosc);
+        System.out.println("Kwadrat liczby x = " + kwadrat);
+
+    }
+
+
+}
+
+
