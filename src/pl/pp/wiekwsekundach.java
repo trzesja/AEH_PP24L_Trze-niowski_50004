ad9cc72ad2b5c42d034a687cb8786a58a49e2fcb
Jakub <trzesja9342_aehit@students.vizja.pl>
2024-03-11 16:57:06 +0100

    Pierwszy zapis do repozytoriumLab2

---
+++ src/pl/pp/wiekwsekundach.java
@@ -0,0 +1,29 @@
+package pl.pp;
+
+import java.util.Scanner;
+
+public class wiekwsekundach {
+    public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("Proszę podać swój wiek w latach: ");
+    int wiekWLatach = scanner.nextInt();
+
+    long wiekWSekundach = (long) (wiekWLatach * 365.25 * 24 * 60 * 60);
+
+    System.out.println("Twój wiek w sekundach wynosi: "+wiekWSekundach +" sekund.");
+
+    scanner.close();
+}
+}
+
+
+
+
+
+
+
+
+
+
