4e3c23e51357c739096250f4090b5d1144a08c0e
Jakub <trzesja9342_aehit@students.vizja.pl>
2024-03-11 17:34:30 +0100

    Pierwszy zapis do repozytoriumLab2

---
+++ src/pl/pp/wiekwsekundach.java
@@ -0,0 +1,27 @@
+package pl.pp;
+
+
+public class wiekwsekundach {
+    public static void main(String[] args) {
+
+        int x = 10;
+
+        int dwukrotnosc = 2 * x;
+
+        int kwadrat = x * x;
+
+        System.out.println("x = " + x);
+        System.out.println("Dwukrotność liczby x = " + dwukrotnosc);
+        System.out.println("Kwadrat liczby x = " + kwadrat);
+}
+}
+
+
+
+
+
+
+
+
+
+
